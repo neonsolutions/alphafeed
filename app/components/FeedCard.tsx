2115c13902c7a16c09d5fe1ca4a30ab325c38dae
lbowles <lspbowles@gmail.com>
2023-05-24 17:01:23 +0200

    source link added

--- app/components/FeedCard.tsx
+++ app/components/FeedCard.tsx
@@ -47,7 +47,7 @@ const FeedCard = ({ title, body, scores, media, mediaType, link }: CardProps) =>
   }
 
   return (
-    <div className="max-w-md mx-auto bg-white rounded-2xl border border-gray-200 md:max-w-2xl m-4 p-6">
+    <div className="max-w-md mx-auto bg-white rounded-2xl border border-gray-200 md:max-w-2xl my-8 p-6">
       <div className="w-full flex justify-between pb-3">
         <div className="flex justify-start gap-3">
           <h3 className="text-[16px] font-medium text-gray-900">{title}</h3>
@@ -58,11 +58,11 @@ const FeedCard = ({ title, body, scores, media, mediaType, link }: CardProps) =>
             onMouseLeave={() => setDropdownVisible(false)} // Hide dropdown on hover exit
           >
             <button className="inline-flex items-center rounded-md bg-gray-100 hover:bg-indigo-100 px-3 py-1 text-xs font-bold text-indigo-700 ring-1 ring-inset ring-indigo-700/30">
-              {scores.significance.toString()}{" "}
+              {scores.significance.toString()}
               <Image alt="downIcon" width={8} height={8} src="/images/feedCard/down.svg" className="ml-1" />
             </button>
             {dropdownVisible && ( // Show dropdown if dropdownVisible is true
-              <div className="origin-top-right absolute left-0 top-6 w-32 rounded-md shadow-xl bg-white ring-1 ring-black ring-opacity-5 transition-all ease-out duration-300 transform opacity-100 scale-100">
+              <div className="origin-top-right z-50 absolute left-0 top-6 w-32 rounded-md shadow-xl bg-white ring-1 ring-black ring-opacity-5 transition-all ease-out duration-300 transform opacity-100 scale-100">
                 <div className="py-1 pb-4" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
                   <p
                     className="flex justify-between items-center px-4 py-2 text-sm text-gray-700 hover:text-gray-900"
@@ -104,16 +104,17 @@ const FeedCard = ({ title, body, scores, media, mediaType, link }: CardProps) =>
 
         <Image src={icon} alt="mediaType icon" width={22} height={22} />
       </div>
-      <p className="text-gray-500 text-sm">{body}</p>
-      <div className="flex overflow-x-scroll space-x-4  py-4">
-        {media! && (
-          <>
-            {media.map((imgUrl, index) => (
-              <img key={index} src={imgUrl} alt={`media ${index}`} className="rounded-lg" />
-            ))}
-          </>
-        )}
-      </div>
+      <p className="text-gray-500 text-sm pb-4">{body}</p>
+
+      {media! && (
+        <div className="flex overflow-x-scroll space-x-4  pb-4">
+          {media.map((imgUrl, index) => (
+            <img key={index} src={imgUrl} alt={`media ${index}`} className="rounded-lg" />
+          ))}
+        </div>
+      )}
+
+      <p className="text-gray-400 text-xs underline">{link}</p>
     </div>
   )
 }
