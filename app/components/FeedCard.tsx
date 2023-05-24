cf14ac5bf7c52264282fe2e3820933fd598e6144
lbowles <lspbowles@gmail.com>
2023-05-24 16:37:00 +0200

    hover score wip

--- app/components/FeedCard.tsx
+++ app/components/FeedCard.tsx
@@ -1,20 +1,28 @@
+import { useState } from "react"
 import Image from "next/image"
 
 type CardProps = {
   title: string
   body: string
-  ranking: number
-  media: string[] // Assume this is an array of URLs for images
+  scores: {
+    significance: number
+    relevance: number
+    impact: number
+    novelty: number
+    reliability: number
+  }
+  media: string[] | undefined
   mediaType: "twitter" | "internet" | "news" | "research"
   link: string
 }
 
-const FeedCard = ({ title, body, ranking, media, mediaType, link }: CardProps) => {
+const FeedCard = ({ title, body, scores, media, mediaType, link }: CardProps) => {
   let icon = "/images/feedCard/internetIcon.svg"
-  console.log(mediaType)
+  const [dropdownVisible, setDropdownVisible] = useState(false) // New state
+
   switch (mediaType) {
     case "twitter":
-      icon = "/images/twitterIcon.svg"
+      icon = "/images/feedCard/twitterIconLight.svg"
       break
     case "internet":
       icon = "/images/feedCard/internetIcon.svg"
@@ -26,19 +34,50 @@ const FeedCard = ({ title, body, ranking, media, mediaType, link }: CardProps) =
       icon = "/images/feedCard/researchIcon.svg"
       break
   }
-  console.log(icon)
+
   return (
     <div className="max-w-md mx-auto bg-white rounded-2xl border border-gray-200 md:max-w-2xl m-4 p-6">
       <div className="w-full flex justify-between pb-3">
-        <h3 className="text-[16px] font-medium text-gray-900">{title}</h3>
+        <div className="flex justify-start gap-3">
+          <h3 className="text-[16px] font-medium text-gray-900">{title}</h3>
+
+          <div
+            className="relative inline-flex"
+            onMouseEnter={() => setDropdownVisible(true)} // Show dropdown on hover
+            onMouseLeave={() => setDropdownVisible(false)} // Hide dropdown on hover exit
+          >
+            <button className="inline-flex items-center rounded-md bg-gray-100 hover:bg-indigo-100 px-3 py-1 text-xs font-bold text-indigo-700 ring-1 ring-inset ring-indigo-700/30">
+              {scores.significance.toString()}{" "}
+              <Image alt="downIcon" width={8} height={8} src="/images/feedCard/down.svg" className="ml-1" />
+            </button>
+            {dropdownVisible && ( // Show dropdown if dropdownVisible is true
+              <div className="origin-top-right absolute left-0 top-6 w-32 rounded-md shadow-xl bg-white ring-1 ring-black ring-opacity-5 transition-all ease-out duration-300 transform opacity-100 scale-100">
+                <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+                  <p className="block px-4 py-2 text-sm text-gray-700  hover:text-gray-900" role="menuitem">
+                    Relevance: {scores.relevance}
+                  </p>
+                  <p className="block px-4 py-2 text-sm text-gray-700  hover:text-gray-900" role="menuitem">
+                    Impact: {scores.impact}
+                  </p>
+                  <p className="block px-4 py-2 text-sm text-gray-700  hover:text-gray-900" role="menuitem">
+                    Novelty: {scores.novelty}
+                  </p>
+                  <p className="block px-4 py-2 text-sm text-gray-700  hover:text-gray-900" role="menuitem">
+                    Reliability: {scores.reliability}
+                  </p>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
         <Image src={icon} alt="mediaType icon" width={22} height={22} />
       </div>
       <p className="text-gray-500 text-sm">{body}</p>
-      <div className="flex overflow-x-scroll space-x-4 pt-4">
-        {media.length !== 0 && (
+      <div className="flex overflow-x-scroll space-x-4  py-4">
+        {media! && (
           <>
             {media.map((imgUrl, index) => (
-              <img key={index} src={imgUrl} alt={`media ${index}`} className="round" />
+              <img key={index} src={imgUrl} alt={`media ${index}`} className="rounded-lg" />
             ))}
           </>
         )}
