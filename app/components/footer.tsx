a597133a28b254f1b12db555acb456d8a60bc7d4
lbowles <lspbowles@gmail.com>
2023-05-25 14:54:21 +0200

    fixed page navigation name

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -6,11 +6,11 @@ export default function Footer() {
   return (
     <div className="w-full px-6 lg:px-8 py-6  border-t-2 border-gray-200 bg-gray-100 text-center ">
       <div className="flex justify-center gap-3 pt-2">
-        <Link href="/privacyPolicy">
+        <Link href="/privacy-policy">
           <p className="text-sm text-gray-400 hover:text-indigo-300 hover:underline">Privacy Policy</p>
         </Link>
         <p className="text-sm text-gray-400 hover:text-indigo-300 ">|</p>
-        <Link href="/termsAndConditions ">
+        <Link href="/terms-and-conditions ">
           <p className="text-sm text-gray-400 hover:text-indigo-300 hover:underline">Terms And Conditions</p>
         </Link>
         <p className="text-sm text-gray-400 hover:text-indigo-300">|</p>
