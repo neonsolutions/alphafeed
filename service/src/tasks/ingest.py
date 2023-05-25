ca6f4fca80e95a5fb32153f411183d4a1ad8c031
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2023-05-25 12:01:56 +0200

    Properly ingest published time

--- service/src/tasks/ingest.py
+++ service/src/tasks/ingest.py
@@ -3,34 +3,43 @@ from models.database import Session
 from models import FeedItem
 import feedparser
 from tqdm import tqdm
+from datetime import datetime
 
 
 def main():
     # Parse the RSS feed
-    print("Parsing RSS feed")
+    print("Fetching RSS feed")
     d = feedparser.parse("https://nitter.net/i/lists/1660595980486492162/rss")
 
-    print("Inserting into the database")
+    print(f"Processing {len(d.entries)} items")
     # Open a new session
     session = Session()
 
+    inserted_count = 0
+    skipped_count = 0
+
     # Loop over each entry
     for entry in d.entries:
+        date_format = "%a, %d %b %Y %H:%M:%S %Z"
+        parsed_time = datetime.strptime(
+            entry.published, date_format)
         feed_item = FeedItem(
-            title=entry.title,
+            title_raw=entry.title,
             link=entry.link,
-            description=entry.description,
-            published=entry.published,
+            description_raw=entry.description,
+            published=parsed_time,
             author=entry.author
         )
 
         session.add(feed_item)
         try:
             session.commit()
-            print(f"Inserted {entry.title[:10]}... into the database.")
+            inserted_count += 1
         except exc.IntegrityError:
-            print(f"{entry.title[:10]}... is already in the database.")
+            skipped_count += 1
             session.rollback()
+
+    print(f"Inserted {inserted_count} items, skipped {skipped_count} items")
 
     # Close the session
     session.close()
