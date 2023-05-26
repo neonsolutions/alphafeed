c772a85329e2ff4ab8e62ce7aad0fbc76c6941a3
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2023-05-26 13:01:14 +0200

    Extract videos

--- app/utils/feed-helpers.ts
+++ app/utils/feed-helpers.ts
@@ -1,10 +1,11 @@
 import { JSDOM } from "jsdom"
 
-export function extractImageSources(text: string): string[] {
+export function extractMediaSources(text: string): string[] {
   try {
     const dom = new JSDOM(text)
     const imgElements = dom.window.document.getElementsByTagName("img")
-    const sources = Array.from(imgElements).map((img) => img.src)
+    const vidElements = dom.window.document.getElementsByTagName("video")
+    const sources = [...Array.from(vidElements).map((vid) => vid.src), ...Array.from(imgElements).map((img) => img.src)]
     return sources
   } catch (error) {
     console.error("Could not parse HTML: ", error)
