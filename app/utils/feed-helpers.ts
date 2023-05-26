eb72da6f689a568fdba3df820f62d489efba39eb
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2023-05-26 13:16:59 +0200

    Extract video sources WIP

--- app/utils/feed-helpers.ts
+++ app/utils/feed-helpers.ts
@@ -4,8 +4,11 @@ export function extractMediaSources(text: string): string[] {
   try {
     const dom = new JSDOM(text)
     const imgElements = dom.window.document.getElementsByTagName("img")
-    const vidElements = dom.window.document.getElementsByTagName("video")
-    const sources = [...Array.from(vidElements).map((vid) => vid.src), ...Array.from(imgElements).map((img) => img.src)]
+    const vidElements = dom.window.document.querySelectorAll("video source")
+    const sources = [
+      ...Array.from(vidElements).map((vid) => (vid as any).src),
+      ...Array.from(imgElements).map((img) => img.src),
+    ]
     return sources
   } catch (error) {
     console.error("Could not parse HTML: ", error)
