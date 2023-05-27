ed11c90c5b431f1d628be4e3a03524587fdc57cf
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2023-05-27 15:09:09 +0200

    Manage billing

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -6,6 +6,22 @@ import { useSession } from "next-auth/react"
 import { useState } from "react"
 import { signOut } from "next-auth/react"
 
+const postData = async ({ url }: { url: string }) => {
+  const res: Response = await fetch(url, {
+    method: "POST",
+    headers: new Headers({ "Content-Type": "application/json" }),
+    credentials: "same-origin",
+  })
+
+  if (!res.ok) {
+    console.log("Error in postData", { url, res })
+
+    throw Error(res.statusText)
+  }
+
+  return res.json()
+}
+
 export default function Navbar() {
   const { data: session, status } = useSession()
   const router = useRouter()
@@ -14,59 +30,17 @@ export default function Navbar() {
 
   const loading = status === "loading"
 
-  let linkElement
-
-  if (asPath === "/") {
-    linkElement = (
-      <Link href="/login">
-        <div className="text-sm font-semibold leading-6 text-gray-900">
-          Log in <span aria-hidden="true">&rarr;</span>
-        </div>
-      </Link>
-    )
-  } else if (asPath === "/feed") {
-    linkElement = (
-      <>
-        <div
-          className=""
-          onMouseEnter={() => setDropdownVisible(true)} // Show dropdown on hover
-          onMouseLeave={() => setDropdownVisible(false)} // Hide dropdown on hover exit
-        >
-          <UserCircleIcon className="h-6 w-10 text-gray-900" />
-          {dropdownVisible && ( // Show dropdown if dropdownVisible is true
-            <div className=" absolute right-8 translate w-32 rounded-md shadow-xl bg-white ring-1 ring-black ring-opacity-5 transition-all ease-out duration-300 transform opacity-100 scale-100 text-gray-900 text-[12px] font-medium">
-              <Link href="/account" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
-                <div role="menuitem" className="flex justify-center items-center py-2 hover:bg-gray-100 rounded-t-md">
-                  Manage billing
-                </div>
-              </Link>
-
-              <Link href="/signout" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
-                <div role="menuitem" className="flex justify-center items-center py-2 hover:bg-gray-100 rounded-b-md">
-                  Sign out
-                </div>
-              </Link>
-            </div>
-          )}
-        </div>
-      </>
-    )
-  } else if (asPath === "/account") {
-    // we are on the account page
-    linkElement = (
-      <Link href="/home">
-        <HomeIcon className="h-6 w-6 text-gray-900" />
-      </Link>
-    )
-  } else if (asPath === "/login" || asPath === "/register") {
-    // we are on the account page
-    linkElement = (
-      <div className="text-sm font-semibold leading-6 text-gray-900">
-        <Link href="/">
-          <span aria-hidden="true">←</span> Back
-        </Link>
-      </div>
-    )
+  const redirectToCustomerPortal = async () => {
+    // setLoading(true);
+    try {
+      const { url, error } = await postData({
+        url: "/api/payment/create-portal-link",
+      })
+      window.location.assign(url)
+    } catch (error) {
+      if (error) return alert((error as Error).message)
+    }
+    // setLoading(false);
   }
 
   return (
@@ -77,7 +51,64 @@ export default function Navbar() {
             <Image src="/images/logoWithText.svg" alt="logoWithText" className="-m-1.5 p-1.5" height={30} width={100} />
           </Link>
         </div>
-        <div className=" lg:flex lg:flex-1 lg:justify-end z-40">{linkElement}</div>
+        <div className=" lg:flex lg:flex-1 lg:justify-end z-40">
+          {session?.user ? (
+            <>
+              <div
+                className=""
+                onMouseEnter={() => setDropdownVisible(true)} // Show dropdown on hover
+                onMouseLeave={() => setDropdownVisible(false)} // Hide dropdown on hover exit
+              >
+                <UserCircleIcon className="h-6 w-10 text-gray-900" />
+                {dropdownVisible && ( // Show dropdown if dropdownVisible is true
+                  <div className=" absolute right-8 translate w-32 rounded-md shadow-xl bg-white ring-1 ring-black ring-opacity-5 transition-all ease-out duration-300 transform opacity-100 scale-100 text-gray-900 text-[12px] font-medium">
+                    <Link
+                      onClick={() => {
+                        redirectToCustomerPortal()
+                      }}
+                      href="#"
+                      role="menu"
+                      aria-orientation="vertical"
+                      aria-labelledby="options-menu"
+                    >
+                      <div
+                        role="menuitem"
+                        className="flex justify-center items-center py-2 hover:bg-gray-100 rounded-t-md"
+                      >
+                        Manage billing
+                      </div>
+                    </Link>
+
+                    <Link
+                      onClick={() => signOut({ callbackUrl: "/" })}
+                      href="#"
+                      role="menu"
+                      aria-orientation="vertical"
+                      aria-labelledby="options-menu"
+                    >
+                      <div
+                        role="menuitem"
+                        className="flex justify-center items-center py-2 hover:bg-gray-100 rounded-b-md"
+                      >
+                        Sign out
+                      </div>
+                    </Link>
+                  </div>
+                )}
+              </div>
+            </>
+          ) : router.asPath !== "/login" ? (
+            <div>
+              <Link href="/login">
+                <div className="text-sm font-semibold leading-6 text-gray-900">
+                  Log in <span aria-hidden="true">&rarr;</span>
+                </div>
+              </Link>
+            </div>
+          ) : (
+            <></>
+          )}
+        </div>
       </nav>
     </header>
   )
--- app/pages/api/payment/create-portal-link.ts
+++ app/pages/api/payment/create-portal-link.ts
@@ -1,41 +1,35 @@
-import { NextApiHandler } from 'next';
-import { createServerSupabaseClient } from '@supabase/auth-helpers-nextjs';
-
-import { stripe } from '@/utils/stripe';
-import { createOrRetrieveCustomer } from '@/utils/supabase-admin';
-import { getURL } from '@/utils/helpers';
+import { NextApiHandler } from "next"
+import { stripe } from "../../../utils/stripe"
+import { createOrRetrieveCustomer } from "../../../utils/stripe-helpers"
+import { getServerSession } from "next-auth"
+import { authOptions } from "../auth/[...nextauth]"
 
 const CreatePortalLink: NextApiHandler = async (req, res) => {
-  if (req.method === 'POST') {
+  const session = await getServerSession(req, res, authOptions)
+
+  if (!session?.user?.email) {
+    return res.status(403).json({ error: { statusCode: 403, message: "Not authorized" } })
+  }
+
+  if (req.method === "POST") {
     try {
-      const supabase = createServerSupabaseClient({ req, res });
-      const {
-        data: { user }
-      } = await supabase.auth.getUser();
+      const customer = await createOrRetrieveCustomer(session?.user?.email)
 
-      if (!user) throw Error('Could not get user');
-      const customer = await createOrRetrieveCustomer({
-        uuid: user.id || '',
-        email: user.email || ''
-      });
-
-      if (!customer) throw Error('Could not get customer');
+      if (!customer) throw Error("Could not get customer")
       const { url } = await stripe.billingPortal.sessions.create({
-        customer,
-        return_url: `${getURL()}/account`
-      });
+        customer: customer.stripeCustomerId!,
+        return_url: `${process.env.SERVER_ENDPOINT}/feed`,
+      })
 
-      return res.status(200).json({ url });
+      return res.status(200).json({ url })
     } catch (err: any) {
-      console.log(err);
-      res
-        .status(500)
-        .json({ error: { statusCode: 500, message: err.message } });
+      console.log(err)
+      res.status(500).json({ error: { statusCode: 500, message: err.message } })
     }
   } else {
-    res.setHeader('Allow', 'POST');
-    res.status(405).end('Method Not Allowed');
+    res.setHeader("Allow", "POST")
+    res.status(405).end("Method Not Allowed")
   }
-};
+}
 
-export default CreatePortalLink;
+export default CreatePortalLink
