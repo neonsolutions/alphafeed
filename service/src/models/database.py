7199b569ff360e5f79f857e954a8ee3deebb2a5e
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2023-05-29 17:32:48 +0200

    Remove old python code

--- service/src/models/database.py
+++
@@ -1,7 +0,0 @@
-import os
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-# Create the SQLAlchemy engine and session
-engine = create_engine(os.environ["DATABASE_URI"])
-Session = sessionmaker(bind=engine)
