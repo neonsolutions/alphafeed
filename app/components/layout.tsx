c4709cc73766452852e1675958060a5f5f4e782a
lbowles <lspbowles@gmail.com>
2023-05-29 15:57:56 +0200

    Dark mode WIP
    Need to change how fetch if dark mode is saved on refresh

--- app/components/layout.tsx
+++ app/components/layout.tsx
@@ -1,6 +1,6 @@
 import Footer from "./footer"
 import Navbar from "./navbar"
-
+import { useEffect, useState } from "react"
 import { DM_Sans } from "next/font/google"
 
 const dmSans = DM_Sans({
@@ -9,9 +9,30 @@ const dmSans = DM_Sans({
 })
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const [darkMode, setDarkMode] = useState<boolean>(false)
+
+  useEffect(() => {
+    if (localStorage.getItem("darkMode") === "true") {
+      setDarkMode(true)
+    }
+  }, [])
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add("dark")
+      localStorage.setItem("darkMode", "true")
+    } else {
+      document.documentElement.classList.remove("dark")
+      localStorage.setItem("darkMode", "false")
+    }
+  }, [darkMode])
+
+  const mainClass = " flex flex-col min-h-screen overflow-hidden"
+
   return (
-    <div className="flex flex-col min-h-screen overflow-hidden">
-      <Navbar />
+    <div className={darkMode ? "dark " + mainClass : mainClass}>
+      <Navbar darkMode={darkMode} setDarkMode={(mode: boolean) => setDarkMode(mode)} />
+
       <main className={dmSans.className + " flex-grow "}>{children}</main>
       <Footer />
     </div>
