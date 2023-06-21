a6bb96f3ea7ffc601d055b25e1075f373a3b0d8d
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2023-06-21 13:33:07 +0200

    Replace google analytics with beampipe

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -37,17 +37,9 @@ function MyApp({ Component, pageProps }: AppProps) {
           <Layout>
             <Script
               strategy="lazyOnload"
-              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+              src={`https://beampipe.io/js/tracker.js`}
+              data-beampipe-domain="alphafeed.xyz"
             />
-            <Script id="ga-analytics">
-              {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
-          `}
-            </Script>
             <Component {...pageProps} />
           </Layout>
         </SessionProvider>
